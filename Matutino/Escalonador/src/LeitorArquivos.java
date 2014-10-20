7292cb17e05c7c9ace01776b118d76067e85da5c
Heloisa Carbone <heloisacarbone@gmail.com>
2014-10-20 12:54:49 -0200

    Arquivos sendo lidos em ordem alfabetica

--- Matutino/Escalonador/src/LeitorArquivos.java
+++ Matutino/Escalonador/src/LeitorArquivos.java
@@ -1,10 +1,12 @@
+import java.awt.List;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.ListIterator;
 
 
 /**
@@ -56,25 +58,30 @@ public class LeitorArquivos {
 		Processo aux;
 		File folder = new File("src/processos_entrada/");
 		String temp = "";
+		LinkedList<String> files = new LinkedList<String>();
 		for (final File fileEntry : folder.listFiles()) {
-
 			if (fileEntry.isFile()) {
 				temp = fileEntry.getName();
-				if (!temp.equals("quantum.txt")) {  
-		
-					int posicao = (Integer.parseInt(temp.split("\\.")[0])) - 1; // Pega o numero do processo e subtrai 1, para termos uma fila a partir do 0
-	
-					LinkedList<String> instrucoes = lerProcesso("src/processos_entrada/" + temp);
-					aux = null;
-					if (instrucoes != null) {
-						aux = new Processo(instrucoes);
-						processos.add(aux);
-						System.out.println(aux.instrucoes.size());
-					}
-					
-				}
+				if (!temp.equals("quantum.txt"))
+					files.add(temp);
 			}
-		}	
+		}
+		Collections.sort(files);
+		
+		ListIterator<String> listIterator = files.listIterator();
+		
+		while (listIterator.hasNext()) {
+			String file = "src/processos_entrada/" + listIterator.next();
+			System.out.println(file);
+            LinkedList<String> instrucoes = lerProcesso(file);
+			aux = null;
+			if (instrucoes != null) {
+				aux = new Processo(instrucoes);
+				processos.add(aux);
+				System.out.println(aux.instrucoes.size());
+			}
+        }
+		//int posicao = (Integer.parseInt(temp.split("\\.")[0])) - 1; // Pega o numero do processo e subtrai 1, para termos uma fila a partir do 0	
 		
 		return processos;
 	}
@@ -93,6 +100,8 @@ public class LeitorArquivos {
  
 			quantum = Integer.parseInt(arquivoProcesso.readLine());
 
+			arquivoProcesso.close();
+			
 			return quantum;
  
 		} catch (IOException e) {
